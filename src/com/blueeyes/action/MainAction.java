69c4f1f966586fdd7441118be0046236c95f16df
hoa minh <hoa minh>
2016-04-28 20:01:41 +0700

    Pure struts

---
+++ src/com/blueeyes/action/MainAction.java
@@ -0,0 +1,14 @@
+package com.blueeyes.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MainAction extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+
+	public String execute() {
+		return SUCCESS;
+	}
+	public void validate() {
+		
+	}
+}
